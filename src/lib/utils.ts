3e928966665e25101d99b240db666cccfb28eede
Eov <egor.paltov@gmail.com>
2024-06-09 10:45:53 +0200

    final changes from v

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { Metadata } from 'next'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -8,18 +9,55 @@ export function cn(...inputs: ClassValue[]) {
 export function formatPrice(
   price: number | string,
   options: {
-    currency?: "EUR",
-    notation?: Intl.NumberFormatOptions["notation"]
+    currency?: 'EUR'
+    notation?: Intl.NumberFormatOptions['notation']
   } = {}
-){
-  const { currency = "EUR", notation = "compact"} = options
+) {
+  const { currency = 'EUR', notation = 'compact' } = options
 
-  const numericPrice = typeof price === "string" ? parseFloat(price) : price
+  const numericPrice =
+    typeof price === 'string' ? parseFloat(price) : price
 
-  return new Intl.NumberFormat("fr-FR", {
-    style: "currency",
+  return new Intl.NumberFormat('fr-FR', {
+    style: 'currency',
     currency,
     notation,
     maximumFractionDigits: 2,
   }).format(numericPrice)
+}
+
+export function constructMetadata({
+  title = 'POMO - marché en ligne de produits locaux et frais.',
+  description = 'Achetez des produits locaux frais sur POMO à Rennes, votre marché en ligne de confiance.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    icons,
+    metadataBase: new URL('https://pomo.today'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
